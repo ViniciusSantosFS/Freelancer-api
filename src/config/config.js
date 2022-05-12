a2d9866cbe017c0d777bcbe87f67ad85c16a3116
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-05-12 20:15:43 -0300

    configura conexão com banco

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dialect: 'postgres',
+  url: process.env.DATABASE_URL,
+  dialectOptions: {
+      ssl: process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false
+    }
+}
