167612225f228f7a77cc2d637cd2757bcf9a9632
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-05-22 20:54:16 -0300

    Cria rota para validar email do usuário

--- src/app.js
+++ src/app.js
@@ -6,5 +6,6 @@ const app = express()
 app.use(express.json())
 app.get('/accounts', accountController.index)
 app.post('/accounts', accountController.store)
+app.put('/accounts/validate-email', accountController.validateEmail)
 
 module.exports = app
